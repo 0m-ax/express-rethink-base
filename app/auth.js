884799222b94d83fd6562d78d2e0c23f0c180874
Max Campbell <maxc@maxc.in>
2016-09-20 22:11:16 +0100

    added signup

--- app/auth.js
+++ app/auth.js
@@ -1,6 +1,16 @@
+const appRoot = require('app-root-path');
+const users = require(appRoot+'/lib/user');
 const express = require('express');
 var app = express.Router();
 app.get('/login',(req,res)=>{
   res.render('login');
+})
+app.get('/signup',(req,res)=>{
+  res.render('signup');
+})
+app.post('/signup',(req,res)=>{
+  users.create(req.body.username,req.body.password)
+  .then(()=>res.redirect('/login'))
+  .catch(()=>res.redirect('/signup?error=true'))
 })
 module.exports= app;
